f3906ef7de6f75b3305a4c647de4b505d3b8d395
nxoim <ghite@nxoim.com>
2025-05-04 22:01:44 +0200

    hello

---
+++ byteCount/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
+}
+
+kotlin {
+    jvm()
+    androidTarget()
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    linuxX64()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(compose.runtime)
+            }
+        }
+    }
+}
+
+android {
+    namespace = "com.nxoim.byteCount"
+    compileSdk = 35
+    defaultConfig {
+        minSdk = 21
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,32 @@
+rootProject.name = "byteCountThing"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        mavenLocal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+include(":byteCount")
